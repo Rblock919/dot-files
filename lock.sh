9821ddaccd066832cec53be21576d17670c752c8
Ryan Block <Ryan.Block93@gmail.com>
2019-06-02 14:08:44 -0400

    added lock screen functionality to desktop ubuntu setup and added commented out lines in the lock script to display over 2 monitors

--- lock.sh
+++ lock.sh
@@ -2,11 +2,13 @@
 
 icon="$HOME/Pictures/lockScreen.png"
 tmpbg='/tmp/screen.png'
-# tmpbg="$HOME/Pictures/macbookBackground.jpg"
 
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+# the following two lines are for my 2 monitor display desktop
+# convert "$tmpbg" "$icon" -gravity center -geometry -1000+0 -composite -matte "$tmpbg"
+# convert "$tmpbg" "$icon" -gravity center -geometry +1000+0 -composite -matte "$tmpbg"
 i3lock -u -i "$tmpbg"
