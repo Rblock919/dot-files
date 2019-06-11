5a9a0b1ccbaa54faaaae425f66cebf75392168ea
Ryan Block <Ryan.Block93@gmail.com>
2019-06-11 16:58:52 -0400

    slightly updated lock.sh for desktop setup

--- lock.sh
+++ lock.sh
@@ -2,6 +2,7 @@
 
 icon="$HOME/Pictures/lockScreen.png"
 tmpbg='/tmp/screen.png'
+# tmpbg="$HOME/Pictures/macbookBackground.jpg"
 
 (( $# )) && { icon=$1; }
 
@@ -9,6 +10,6 @@ scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 # the following two lines are for my 2 monitor display desktop
-# convert "$tmpbg" "$icon" -gravity center -geometry -1000+0 -composite -matte "$tmpbg"
-# convert "$tmpbg" "$icon" -gravity center -geometry +1000+0 -composite -matte "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -geometry -1000+0 -composite -matte "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -geometry +950+0 -composite -matte "$tmpbg"
 i3lock -u -i "$tmpbg"
