49da3ef3774b7bcbc96b9945c5660937b7c52c4d
Ryan Block <Ryan.Block93@gmail.com>
2020-02-01 18:17:28 -0500

    updated desktop config with new monitors and new cursor theme

--- lock.sh
+++ lock.sh
@@ -2,14 +2,17 @@
 
 icon="$HOME/Pictures/lockScreen.png"
 tmpbg='/tmp/screen.png'
-# tmpbg="$HOME/Pictures/macbookBackground.jpg"
 
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-# the following two lines are for my 2 monitor display desktop
-#convert "$tmpbg" "$icon" -gravity center -geometry -1000+0 -composite -matte "$tmpbg"
-#convert "$tmpbg" "$icon" -gravity center -geometry +950+0 -composite -matte "$tmpbg"
+
+# Laptop
+# convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+
+# Dual Monitor Desktop
+convert "$tmpbg" "$icon" -gravity center -geometry -600-250 -composite -matte "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -geometry +1300+0 -composite -matte "$tmpbg"
+
 i3lock -u -i "$tmpbg"
