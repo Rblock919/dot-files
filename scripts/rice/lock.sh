66c283665dead7ff35e3f49094e1bf27a14fba89
Ryan Block <ryan.block93@gmail.com>
2020-10-22 10:36:01 -0400

    updated lock.sh script to remove /tmp/screenshot.png first

--- scripts/rice/lock.sh
+++ scripts/rice/lock.sh
@@ -5,6 +5,10 @@ tmpbg='/tmp/screen.png'
 
 (( $# )) && { icon=$1; }
 
+if [ -f $tmpbg ] ; then
+    rm $tmpbg
+fi
+
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 
@@ -12,7 +16,7 @@ convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 # convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 
 # Dual Monitor Desktop
-convert "$tmpbg" "$icon" -gravity center -geometry -600-250 -composite -matte "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -geometry +1300+0 -composite -matte "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -geometry -975-0 -composite -matte "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -geometry +1300-150 -composite -matte "$tmpbg"
 
 i3lock -u -i "$tmpbg"
